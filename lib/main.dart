044b42edc8a0eca6091bb612d40314a0e0b3398a
Arjun <arjundoel@gmail.com>
2022-01-03 09:36:08 +0000

    installed hex color and changed bg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:kitchen_space_2/meals_view.dart';
 
 void main() => runApp(const HomeView());
@@ -6,12 +7,10 @@ void main() => runApp(const HomeView());
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
-  final backGround = const Color(0xFFF89921);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: backGround),
+      theme: ThemeData(scaffoldBackgroundColor: HexColor('#f7ede2')),
       title: 'Kitchen_Space 1',
       home: MealView(),
     );
