079bdeba4c7d8586b89f61ba71c3192dc163ecaa
Arjun <arjundoel@gmail.com>
2022-01-03 16:47:28 +0000

    updated container in meal view

---
+++ lib/components/meal_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MealCard extends StatefulWidget {
+  const MealCard({Key? key}) : super(key: key);
+
+  @override
+  _MealCardState createState() => _MealCardState();
+}
+
+class _MealCardState extends State<MealCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/components/meals_view.dart
+++ lib/components/meals_view.dart
@@ -15,71 +15,81 @@ class MealView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100),
-          ),
-          height: 550,
-          child: FutureBuilder(
-              future: httpService.getMeals(),
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<Meal>> snapshot) {
-                if (snapshot.hasData) {
-                  List<Meal>? meals = snapshot.data;
-                  return ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: meals!.length,
-                      itemBuilder: (BuildContext context, index) => Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                            margin: const EdgeInsets.all(10),
-                            color: HexColor(cardBg),
-                            child: SizedBox(
-                              width: 300,
-                              child: Column(
-                                children: [
-                                  SizedBox(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(0),
-                                      child: ClipRRect(
-                                        borderRadius: const BorderRadius.only(
-                                          topLeft: Radius.circular(15),
-                                          topRight: Radius.circular(15),
-                                        ),
-                                        child: Image.network(
-                                          snapshot.data![index].image,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 130,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+              ),
+              height: 550,
+              child: FutureBuilder(
+                  future: httpService.getMeals(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Meal>> snapshot) {
+                    if (snapshot.hasData) {
+                      List<Meal>? meals = snapshot.data;
+                      return ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: meals!.length,
+                          itemBuilder: (BuildContext context, index) => Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                margin: const EdgeInsets.all(10),
+                                color: HexColor(cardBg),
+                                child: SizedBox(
+                                  width: 300,
+                                  child: Column(
+                                    children: [
+                                      SizedBox(
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(0),
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                const BorderRadius.only(
+                                              topLeft: Radius.circular(15),
+                                              topRight: Radius.circular(15),
+                                            ),
+                                            child: Image.network(
+                                              snapshot.data![index].image,
+                                            ),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Row(
-                                    children: [
-                                      const Padding(
-                                          padding: EdgeInsets.all(8),
-                                          child: Icon(Icons.favorite_outline)),
-                                      Text(snapshot.data![index].name),
+                                      const SizedBox(height: 10),
+                                      Row(
+                                        children: [
+                                          const Padding(
+                                              padding: EdgeInsets.all(8),
+                                              child:
+                                                  Icon(Icons.favorite_outline)),
+                                          Text(snapshot.data![index].name),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          const Padding(
+                                              padding: EdgeInsets.all(8),
+                                              child: Icon(Icons.location_on)),
+                                          Text(snapshot.data![index].location),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 75),
+                                      const Center(
+                                          child: Icon(Icons.reorder_sharp))
                                     ],
                                   ),
-                                  Row(
-                                    children: [
-                                      const Padding(
-                                          padding: EdgeInsets.all(8),
-                                          child: Icon(Icons.location_on)),
-                                      Text(snapshot.data![index].location),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 75),
-                                  const Center(child: Icon(Icons.reorder_sharp))
-                                ],
-                              ),
-                            ),
-                          ));
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              }),
+                                ),
+                              ));
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  }),
+            ),
+          ],
         ),
       ),
     );
