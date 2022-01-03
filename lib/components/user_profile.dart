a0ab7d81c6c3db587593ad12c3c00d1e4c61e1ea
Arjun <arjundoel@gmail.com>
2022-01-03 18:25:21 +0000

    Revert nav

--- lib/components/user_profile.dart
+++ lib/components/user_profile.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
-import 'package:kitchen_space_2/components/bottom_bar.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({Key? key}) : super(key: key);
@@ -10,22 +8,13 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
-  final searchIcon = '#84a59d';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('User Profile'),
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: HexColor(searchIcon),
-          onPressed: () {},
-          child: const Icon(Icons.search), //icon inside button
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: BottomBar(),
       ),
     );
   }
