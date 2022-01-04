b988064d9664d0605b0e4bde1450067212699cc7
Arjun <arjundoel@gmail.com>
2022-01-04 20:31:12 +0000

    popup working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:kitchen_space_2/components/bottom_bar.dart';
 import 'package:kitchen_space_2/components/meals_view.dart';
 
@@ -18,27 +19,26 @@ class HomeView extends StatelessWidget {
       title: 'Kitchen_Space 1',
       home: Scaffold(
         body: MealView(),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: HexColor(searchIcon),
-          onPressed: () => testAlert(context),
-          child: const Icon(Icons.search), //icon inside button
+        floatingActionButton: Builder(
+          builder: (context) => FloatingActionButton(
+            backgroundColor: HexColor(searchIcon),
+            onPressed: () {
+              AwesomeDialog(
+                context: context,
+                dialogType: DialogType.INFO,
+                animType: AnimType.BOTTOMSLIDE,
+                title: 'Dialog Title',
+                desc: 'Dialog description here.............',
+                btnCancelOnPress: () {},
+                btnOkOnPress: () {},
+              ).show();
+            },
+            child: const Icon(Icons.search), //icon inside button
+          ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         bottomNavigationBar: const BottomBar(),
       ),
     );
-  }
-
-  void testAlert(BuildContext context) {
-    var alert = const AlertDialog(
-      title: Text("Test"),
-      content: Text("Done..!"),
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
   }
 }
